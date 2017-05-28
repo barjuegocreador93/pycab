bf04ac7ff768690df43d717cdbcec55efb5af18d
calimpio <barbosa93cg@gmail.com>
2017-05-28 08:04:02 -0500

    problem intalled resolved

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 import resources.packages as File
-from resources.requires import required
+
 
 if File.pipinstall('bottle'):
 	if File.pipinstall('fileDownloader.py') and File.pipinstall('sqlalchemy') and File.pipinstall('oauth2'):
